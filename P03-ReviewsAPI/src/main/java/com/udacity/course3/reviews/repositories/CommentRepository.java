ec8a9f6ddcbdf137730d38dcf51adc9b761ca4fd
Apoorva Lonkar <apoorvalonkar3@gmail.com>
2019-11-24 17:41:33 -0500

    Update CommentRepository.java

--- P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/repositories/CommentRepository.java
+++ P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/repositories/CommentRepository.java
@@ -3,12 +3,13 @@ package com.udacity.course3.reviews.repositories;
 
 import com.udacity.course3.reviews.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-@Repository
+
 public interface CommentRepository extends JpaRepository<Comment,Integer> {
 
+    @Query("SELECT c FROM Comment c WHERE c.review.reviewId = :reviewId")
     List<Comment> findAllByReviewId (Integer reviewId);
 }
