c854c4c7fe69291e9ae0e169726ccae6dea968a2
Apoorva Lonkar <apoorvalonkar3@gmail.com>
2019-11-24 17:42:17 -0500

    Update ReviewRepository.java

--- P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/repositories/ReviewRepository.java
+++ P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/repositories/ReviewRepository.java
@@ -3,12 +3,13 @@ package com.udacity.course3.reviews.repositories;
 
 import com.udacity.course3.reviews.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-@Repository
+
 public interface ReviewRepository extends JpaRepository<Review,Integer> {
 
+    @Query("SELECT r from Review r where r.product.productId = :productId")
     List<Review> findAllByProductId(Integer productId);
 }
